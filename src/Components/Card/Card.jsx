c30fd1cb9eb886b633c50faf3bd3733641c1ba45
Eslam Elgharbawy <eslamelsayd334@gmail.com>
2025-11-25 00:02:44 +0200

    Completed the Projects Section

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -13,46 +13,10 @@ export default function Card({ icon, projects, title, body }) {
                 {title}
               </h3>
             </div>
-            <div className="Card-body mb-8 text-lg text-[#767682]">
+            <div className="Card-body mb-4 text-lg text-[#767682]">
               <p>{body}</p>
             </div>
-            <div className="Explore">
-              <p className="text-[#12103e] pr-8">Explore</p>
-              <div className="Explore-icon group">
-                <span className="inline-block transition-all duration-300 group-hover:translate-y-[-20px] group-hover:translate-x-[20px]">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={3}
-                    stroke="currentColor"
-                    className="size-4 text-[#ff6d5a]"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
-                </span>
-                <span className="absolute translate-x-[-20px] translate-y-[20px] flex items-center justify-center opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:translate-x-0 group-hover:opacity-100 ">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={3}
-                    stroke="currentColor"
-                    className="size-4 text-[#ff6d5a]"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </div>
+          
           </div>
         </div>
       </div>
---
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,65 @@
+export default function ProjectCard({
+  project_image,
+  Link,
+  project_name,
+  description,
+}) {
+  return (
+    <>
+      <div className="min-h-[400px]">
+        <div className="Card_hover p-4 bg-white rounded-lg relative">
+          <a href={Link} target="_blank" rel="noopener noreferrer">
+            <div className="relative overflow-hidden rounded-lg  group">
+              <img
+                src={project_image}
+                alt={project_name}
+                className="w-full object-cover transition-all duration-[0.35s] ease-linear group-hover:scale-105"
+              />
+
+              <div className="absolute inset-0 bg-black/0 group-hover:bg-black/30 transition-all duration-300"></div>
+            </div>
+          </a>
+          <div className="flex justify-between items-center mt-5">
+            <div>
+              <h1 className="font-Lexend text-[25px] font-medium   text-[#12103e]">
+                {project_name}
+              </h1>
+              <p className="relative font-Jost w-fit text-[#767682] font-medium">
+                {description}
+              </p>
+            </div>
+            <a
+              href={Link}
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="w-10 h-10 bg-[#ff6d5a1a] overflow-hidden transition-all duration-300 ease-in-out text-[#ff6d5a] flex justify-center items-center rounded-full"
+            >
+              <svg
+                width="11"
+                height="11"
+                viewBox="0 0 11 11"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1 10L10 1"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+                <path
+                  d="M1 1H10V10"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
